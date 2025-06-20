96685dae8b8ea5dd4afa77b0d674765ed19c11df
azrim <mirzaspc@gmail.com>
2025-06-21 01:47:56 +0700

    pages: attemp to fix padding 2

--- src/pages/Mint.tsx
+++ src/pages/Mint.tsx
@@ -71,7 +71,7 @@ const Mint = () => {
     <div>
       <Navigation />
 
-      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-2 pb-6">
+      <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 pt-2 pb-6 mt-16">
         {/* Main NFT Mint Card */}
         <Card className="bg-card border-border">
           <CardContent className="p-8">
